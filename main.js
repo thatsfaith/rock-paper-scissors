fa1d12f341cd76fb0a5c6d5f4820c174930f805f
Faith <thatsfaith@outlook.com>
2023-06-01 14:54:27 -0500

    added function that plays a single round

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
+const choices = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
-console.log(getComputerChoice());
+
+function checkWinner(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "Tie";
+  } else if (
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "rock" && computerSelection == "paper")
+  ) {
+    return "Player";
+  } else {
+    return "Computer";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  const result = checkWinner(playerSelection, computerSelection);
+  if (result == "Tie") {
+    return "It's a Tie!";
+  } else if (result == "Player") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `Sorry, you lose ${computerSelection} beats ${playerSelection}`;
+  }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
